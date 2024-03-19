502769087a63593a7c0cc859c11cf77511bdf2d9
MusaTamzid05 <tamzid101@gmail.com>
2024-03-20 00:57:39 +0600

    Handle missing value number

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from mflow.preprocessor import DataFramePreprocessor
 if __name__ == "__main__":
     df = pd.read_csv("buggy.csv")
     preprocessor = DataFramePreprocessor(df=df)
-    preprocessor.info()
-    preprocessor.remove_missing_values()
-    print("===============")
-    print(preprocessor.df.isnull().sum())
-    print(preprocessor.df.head())
+    #preprocessor.info()
+    #preprocessor.remove_missing_values()
+    #print(preprocessor.df.isnull().sum())
+    print(preprocessor.handle_all_missing_values())
--- mflow/preprocessor.py
+++ mflow/preprocessor.py
@@ -1,9 +1,17 @@
-
+from sklearn.impute import SimpleImputer
+import numpy as np
 
 class DataFramePreprocessor:
     def __init__(self, df):
         self.df = df
         self.original_df = df.copy()
+
+        col_names = self.df.columns
+        self.data_types = {}
+
+        for index, dt in enumerate(self.df.dtypes):
+            self.data_types[col_names[index]] = dt
+
 
 
     def info(self):
@@ -25,6 +33,12 @@ class DataFramePreprocessor:
         print("************* Null Count **************")
         print(f"{self.df.isnull().sum()}")
 
+    def handle_all_missing_values(self):
+        self.handle_missing_value_numbers()
+        self.remove_missing_values()
+        print(self.df.isnull().sum())
+
+
 
     def remove_missing_values(self, remove_name="row"):
         if remove_name == "row":
@@ -33,5 +47,53 @@ class DataFramePreprocessor:
 
         # column
         self.df = self.df.dropna(axis=1)
+
+
+
+    def handle_missing_value_numbers(self, strategy_name="mean"):
+        num_col_flags = []
+
+        for type_ in self.data_types.values():
+            flag = False
+            if type_ != "object":
+                flag = True
+
+            num_col_flags.append(flag)
+
+        all_cols = list(self.data_types.keys())
+        num_cols = []
+
+        for index, flag in enumerate(num_col_flags):
+            if flag == False:
+                continue
+
+            num_cols.append(all_cols[index])
+
+        imputer = SimpleImputer(missing_values=np.nan, strategy=strategy_name)
+        imputer.fit(self.df[num_cols].values)
+        self.df[num_cols] = imputer.transform(self.df[num_cols])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
