3f986c4bcbe28a29363ae488d5ec12b6051bd2f2
MusaTamzid05 <tamzid101@gmail.com>
2024-03-20 01:10:58 +0600

    Add Label Encoder

--- main.py
+++ main.py
@@ -5,7 +5,5 @@ from mflow.preprocessor import DataFramePreprocessor
 if __name__ == "__main__":
     df = pd.read_csv("buggy.csv")
     preprocessor = DataFramePreprocessor(df=df)
-    #preprocessor.info()
-    #preprocessor.remove_missing_values()
-    #print(preprocessor.df.isnull().sum())
-    print(preprocessor.handle_all_missing_values())
+    #preprocessor.handle_all_missing_values()
+    print(preprocessor.encode_text_data())
--- mflow/preprocessor.py
+++ mflow/preprocessor.py
@@ -1,4 +1,5 @@
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder
 import numpy as np
 
 class DataFramePreprocessor:
@@ -72,6 +73,44 @@ class DataFramePreprocessor:
         imputer = SimpleImputer(missing_values=np.nan, strategy=strategy_name)
         imputer.fit(self.df[num_cols].values)
         self.df[num_cols] = imputer.transform(self.df[num_cols])
+
+
+    def encode_text_data(self, strategy_name="label_encoder"):
+        text_col_flags = []
+
+        for type_ in self.data_types.values():
+            flag = False
+            if type_ == "object":
+                flag = True
+
+            text_col_flags.append(flag)
+
+        all_cols = list(self.data_types.keys())
+        text_cols = []
+
+        for index, flag in enumerate(text_col_flags):
+            if flag == False:
+                continue
+
+            text_cols.append(all_cols[index])
+
+        print(text_cols)
+
+        if strategy_name == "label_encoder":
+            self.label_encoders = {}
+
+            for col_name in text_cols:
+                encoder = LabelEncoder()
+                self.df[col_name] = encoder.fit_transform(self.df[col_name].values)
+                self.label_encoders[col_name] = encoder
+
+
+            print(self.df.head())
+
+
+
+
+
 
 
 
